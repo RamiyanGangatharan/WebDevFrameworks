3483aa1de52f34fca9ae96169aaf7d72bd9d0288
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-10-08 21:04:01 -0400

    reset with 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("./src/img");
+    // eleventyConfig.setTemplateFormats(["md", "ejs", "njk"]);
+
+    return{
+        dir:{
+            output: "dist",
+            input: "src" 
+        }
+    }
+}
